41c1f4e10a9f4ad97f8c93879f9818f0cd8aaa35
Bunyamin Kilicer <bunyaminkilicer@192.168.1.106>
2023-04-01 23:31:10 +0300

    Route added for existing pages, 404 page added.

---
+++ oep/src/pages/student/StudentExamsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function StudentExamsPage() {
+  return (
+    <Container>
+      <p>text</p>
+    </Container>
+  );
+}
+
+export default StudentExamsPage;
