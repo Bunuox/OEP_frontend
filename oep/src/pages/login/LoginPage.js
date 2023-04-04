191ec618975e89a5894f279762b0edb1fe3f1bde
bunuox <bunyamin.klcr@outlook.com>
2023-04-04 22:45:28 +0300

    Login page updated for fetching user api

--- oep/src/pages/login/LoginPage.js
+++ oep/src/pages/login/LoginPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
 import LoginForm from "../../components/form/LoginForm";
@@ -10,14 +10,34 @@ import { useNavigate } from "react-router-dom";
 function LoginPage() {
   const navigate = useNavigate();
   const { setUser } = useStudentAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const loginHandle = () => {
-    console.log("txt");
-    setUser({
-      id: 1,
-      username: "bunyaminkilicer",
-    });
-    navigate("/student");
+  const loginHandle = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:8081/student/findStudent", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setUser({
+          studentId: resJson.studentId,
+        });
+        navigate("/student");
+      } else {
+        console.log("başarısız");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -25,8 +45,22 @@ function LoginPage() {
       <OepBrand />
       <LoginForm>
         <h3>Giriş yap</h3>
-        <Input id="user_mail" type={"text"} placeholder={"Email"} />
-        <Input id="user_password" type={"password"} placeholder={"Şifre"} />
+        <Input
+          id="user_mail"
+          type={"text"}
+          placeholder={"Email"}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <Input
+          id="user_password"
+          type={"password"}
+          placeholder={"Şifre"}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <Button
           type="submit"
           onClick={loginHandle}
