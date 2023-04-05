3e2636f1209f45eb024ae72130fe06970b012cf2
bunuox <bunyamin.klcr@outlook.com>
2023-04-05 23:56:35 +0300

    teacher login page added

--- oep/src/pages/login/TeacherLoginPage.js
+++ oep/src/pages/login/TeacherLoginPage.js
@@ -4,46 +4,15 @@ import Button from "../../components/button/Button";
 import LoginForm from "../../components/form/LoginForm";
 import OepBrand from "../../components/Brand/OepBrand";
 import { BsLock } from "react-icons/bs";
-import { useStudentAuth } from "../../components/context/AuthStudentContext";
-import { useNavigate } from "react-router-dom";
 
-function LoginPage() {
-  const navigate = useNavigate();
-  const { setUser } = useStudentAuth();
+function TeacherLoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const loginHandle = async (e) => {
-    e.preventDefault();
-    try {
-      let res = await fetch("http://localhost:8081/student/findStudent", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setUser({
-          studentId: resJson.studentId,
-        });
-        navigate("/student");
-      } else {
-        console.log("başarısız");
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
   return (
     <body>
       <OepBrand />
-      <LoginForm>
+      <LoginForm className="teacher">
         <h3>Giriş yap</h3>
         <Input
           id="user_mail"
@@ -63,7 +32,7 @@ function LoginPage() {
         />
         <Button
           type="submit"
-          onClick={loginHandle}
+          onClick={() => console.log("login")}
           className={"button-secondary"}
         >
           Giriş
@@ -79,4 +48,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default TeacherLoginPage;
